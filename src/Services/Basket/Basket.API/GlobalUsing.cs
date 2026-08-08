54a5eb84c3c3b3b28bd870e67f5afcbc0ade7a39
Andres Salinas <andresali1@hotmail.es>
2026-08-08 13:37:47 +0200

    Implementing redis cache

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -9,3 +9,5 @@ global using FluentValidation;
 global using Mapster;
 global using Marten;
 global using MediatR;
+global using Microsoft.Extensions.Caching.Distributed;
+global using System.Text.Json;
