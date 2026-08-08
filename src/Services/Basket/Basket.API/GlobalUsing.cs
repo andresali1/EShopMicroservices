d08dfa30c0723709c4b3fd15d19f9ba822e943e0
Andres Salinas <andresali1@hotmail.es>
2026-08-08 11:21:16 +0200

    Add global exception handling in Basket API

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -3,6 +3,7 @@ global using Basket.API.Exceptions;
 global using Basket.API.Models;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions.Handler;
 global using Carter;
 global using FluentValidation;
 global using Mapster;
