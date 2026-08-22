c9c86ce6bc8cbc86aba94b221daca7409380101e
Andres Salinas <andresali1@hotmail.es>
2026-08-22 14:52:59 +0200

    Adding Domain layer infrastructure

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Domain.Abstractions
+{
+    public interface IDomainEvent : INotification
+    {
+        Guid EventId => Guid.NewGuid();
+        public DateTime OcurredOn => DateTime.Now;
+        public string EventType => GetType().AssemblyQualifiedName;
+    }
+}
